e0a1c5ea751ee341079acd7909edbdceb3773b63
albhasan <albhasan@gmail.com>
2019-01-10 09:39:34 -0200

    Fixed error in documentation. Documentation updated

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' @title Sample time-series from the PRODES system using interpolation.
+#' @title Sample time-series from the PRODES system using a fusion model. 
 #'
 #' @description A dataset containing a tibble with time series sampled on the
 #' brazilian Amazon. The time series come from Landsat 8 Collection images. The
@@ -6,6 +6,47 @@
 #' StarFM builds a statistical model between MODIS and LANDSAT images at time
 #' t1 which is later applied to MODIS images at time t0. The result is a
 #' prediction of a Landsat 8 image at t0.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name prodes_samples_starfm
+#' @usage data(prodes_samples_starfm)
+#'
+#' @format A tibble with 1 rows and 7 variables: (a) longitude: East-west coordinate of the time series sample (WGS 84);
+#'   latitude (North-south coordinate of the time series sample in WGS 84), start_date (initial date of the time series),
+#'   end_date (final date of the time series), label (the class label associated to the sample),
+#'   coverage (the name of the coverage associated with the data),
+#'   time_series (list containing a tibble with the values of the time series).
+NULL
+
+#' @title Sample time-series from the PRODES system using a fusion model. 
+#'
+#' @description A dataset containing a tibble with time series sampled on the
+#' brazilian Amazon. The time series come from Landsat 8 Collection images. The
+#' clouds in the images are filled in using the StarFM image fusion model.
+#' StarFM builds a statistical model between MODIS and LANDSAT images at time
+#' t1 which is later applied to MODIS images at time t0. The result is a
+#' prediction of a Landsat 8 image at t0. These time series are the last four
+#' time steps of the samples in prodes_samples_starfm.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name prodes_samples_starfm_few_clouds 
+#' @usage data(prodes_samples_starfm_few_clouds)
+#'
+#' @format A tibble with 1 rows and 7 variables: (a) longitude: East-west coordinate of the time series sample (WGS 84);
+#'   latitude (North-south coordinate of the time series sample in WGS 84), start_date (initial date of the time series),
+#'   end_date (final date of the time series), label (the class label associated to the sample),
+#'   coverage (the name of the coverage associated with the data),
+#'   time_series (list containing a tibble with the values of the time series).
+NULL
+
+#' @title Sample time-series from the PRODES system using interpolation.
+#'
+#' @description A dataset containing a tibble with time series sampled on the
+#' brazilian Amazon. The time series come from Landsat 8 Collection images. The
+#' clouds in the images are filled in using billinear resampled MODIS images
+#' (MOD13Q1 and MYD13Q1) of the closest in terms of place and date.
 #'
 #' @docType data
 #' @keywords datasets
@@ -19,17 +60,19 @@
 #'   time_series (list containing a tibble with the values of the time series).
 NULL
 
-#' @title Sample time-series from the PRODES system using a fusion model.
+#' @title Sample time-series from the PRODES system using interpolation.
 #'
 #' @description A dataset containing a tibble with time series sampled on the
 #' brazilian Amazon. The time series come from Landsat 8 Collection images. The
 #' clouds in the images are filled in using billinear resampled MODIS images
-#' (MOD13Q1 and MYD13Q1) of the closest in terms of place and date.
+#' (MOD13Q1 and MYD13Q1) of the closest in terms of place and date. These time 
+#' series are the last four time steps of the samples in prodes_samples_interpolated.
+
 #'
 #' @docType data
 #' @keywords datasets
-#' @name prodes_samples_starfm
-#' @usage data(prodes_samples_starfm)
+#' @name prodes_samples_interpolated_few_clouds 
+#' @usage data(prodes_samples_interpolated_few_clouds)
 #'
 #' @format A tibble with 1 rows and 7 variables: (a) longitude: East-west coordinate of the time series sample (WGS 84);
 #'   latitude (North-south coordinate of the time series sample in WGS 84), start_date (initial date of the time series),
@@ -37,3 +80,4 @@ NULL
 #'   coverage (the name of the coverage associated with the data),
 #'   time_series (list containing a tibble with the values of the time series).
 NULL
+
