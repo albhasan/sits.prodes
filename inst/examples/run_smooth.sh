ba3116a55aba8eb1f0c4ad42e719abac9645217a
albhasan <albhasan@gmail.com>
2019-02-13 16:33:47 -0200

    first commit

--- inst/examples/run_smooth.sh
+++ inst/examples/run_smooth.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-parallel -j 4 Rscript smooth_results.R --experiment {1} --win_size {2} --noise 10 --overwrite TRUE ::: rep_prodes_50 rep_prodes_51 rep_prodes_52 ::: 3 5 7 
+# parallel -j 4 Rscript smooth_results.R --experiment {1} --win_size {2} --noise 10 --overwrite TRUE ::: rep_prodes_50 rep_prodes_51 rep_prodes_52 ::: 3 5 7 
+parallel -j 4 Rscript smooth_results.R --experiment {1} --win_size {2} --noise 10 --overwrite TRUE ::: rep_prodes_41 rep_prodes_42 ::: 3 5 7 
 
