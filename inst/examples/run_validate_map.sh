e19620e1f7d4751dc70455020556dc340c3763b9
albhasan <albhasan@gmail.com>
2019-02-19 18:08:08 -0300

    call updated

--- inst/examples/run_validate_map.sh
+++ inst/examples/run_validate_map.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-parallel -j 8 Rscript validate_map.R --experiment {1} --smooth_dir {2} ::: rep_prodes_40 rep_prodes_41 rep_prodes_42 rep_prodes_50 rep_prodes_51 rep_prodes_52 ::: smooth_3x3_n10 smooth_5x5_n10 smooth_7x7_n10
+#parallel -j 8 Rscript validate_map.R --experiment {1} --algorithm {2} --smooth_dir {3} ::: rep_prodes_40 rep_prodes_41 rep_prodes_42 rep_prodes_50 rep_prodes_51 rep_prodes_52 ::: dl svm rf  ::: smooth_3x3_n10 smooth_5x5_n10 smooth_7x7_n10
+parallel -j 8 Rscript validate_map.R --experiment {1} --algorithm {2} --smooth_dir {3} ::: rep_prodes_40 rep_prodes_41 rep_prodes_42 rep_prodes_50 rep_prodes_51 rep_prodes_52 ::: svm rf  ::: smooth_3x3_n10 smooth_5x5_n10 smooth_7x7_n10
+
+echo "Finished"
 
