5a51fc43c8e640b7927b67ed27b06dccfa367df6
albhasan <albhasan@gmail.com>
2019-02-23 19:20:35 -0300

    bug fixed. Switch from algorithm to out_dir to avoid recomputing the same vote many times

--- inst/examples/ensemble_vote.R
+++ inst/examples/ensemble_vote.R
@@ -11,8 +11,8 @@ base_path <- "/home/alber/Documents/data/experiments/prodes_reproduction"
 # get arguments ----
 option_list = list(
   make_option("--experiment", type = "character", default = NULL, help = "Name of an experiment e.g. 'rep_prodes_40'", metavar="character"),
-  make_option("--algorithm",  type = "character", default = NULL, help = "Name of an algorithm e.g. 'dl'", metavar="character"),
   make_option("--smooth_dir", type = "character", default = NULL, help = "Name of a smooth directory e.g. 'smooth_3x3_n10'", metavar="character"),
+  make_option("--out_dir",    type = "character", default = NULL, help = "Path to a directory.", metavar="character"),
   make_option("--cores",      type = "integer",   default = 1,    help = "Number of cores for running in parallel [default %default]", metavar="character")
 )
 opt_parser <- OptionParser(option_list = option_list)
@@ -22,14 +22,14 @@ if (length(opt) != 5 || sum(sapply(opt, is.null)) != 0){
   stop("Wrong arguments!")
 }
 experiment <- opt$experiment # "rep_prodes_40"
-algorithm  <- opt$algorithm  # "dl"
 smooth_dir <- opt$smooth_dir # "smooth_3x3_n10"
+out_dir    <- opt$out_dir    # "/net/150.163.2.206/disks/d6/shared/alber/prodes_reproduction/03_classify/rep_prodes_40/results_vote"
 cores      <- opt$cores      # 8
 
 dl_dir  <- file.path(base_path, "03_classify", experiment, "results_dl",  smooth_dir)
 rf_dir  <- file.path(base_path, "03_classify", experiment, "results_rf",  smooth_dir)
 svm_dir <- file.path(base_path, "03_classify", experiment, "results_svm", smooth_dir)
-stopifnot(all(vapply(c(base_path, dl_dir, rf_dir, svm_dir), dir.exists, logical(1))))
+stopifnot(all(vapply(c(out_dir, base_path, dl_dir, rf_dir, svm_dir), dir.exists, logical(1))))
 
 img_pattern <- "^l8_(simple|maskcloud)_[0-9]{6}_[0-9]{4}_(dl|rf|svm)_[0-9]{4}_[0-9]_[0-9]{4}_[0-9].tif"
 
@@ -83,9 +83,6 @@ Mode_count <- function(x) {
 
 raster_mode_ls      <- parallel::mclapply(stack_ls, raster::calc, fun = Mode, mc.cores = cores)
 raster_mode_freq_ls <- parallel::mclapply(stack_ls, raster::calc, fun = Mode_count, mc.cores = cores)
-
-out_dir <- file.path(dl_dir, "ensemble_vote")
-if(!dir.exists(out_dir)) dir.create(out_dir)
 
 for(i in seq_along(raster_mode_ls)){
     fname <- raster_tb %>% dplyr::select(dl_file) %>% dplyr::slice(i) %>% unlist() %>% basename() %>% stringr::str_replace("dl", "dl-rf-svm") %>% stringr::str_replace(".tif", '')
