a3118fffcd1c63a752b2140bc92aa43a78f54855
albhasan <albhasan@gmail.com>
2019-06-13 22:52:06 -0300

    Added validation of non-smoothed maps.

--- inst/examples/run_validate_map.sh
+++ inst/examples/run_validate_map.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# parallel -j 8 --no-notice Rscript validate_map.R  --in_dir /home/alber/Documents/data/experiments/prodes_reproduction/03_classify/{1}/results_vote/{2} --label_file /home/alber/Documents/data/experiments/prodes_reproduction/03_classify/{1}/results_dl/int_labels.csv --out_dir /home/alber/Documents/data/experiments/prodes_reproduction/03_classify/{1}/results_vote/{2}/validation ::: rep_prodes_40 rep_prodes_41 rep_prodes_42 rep_prodes_50 rep_prodes_51 rep_prodes_52 ::: smooth_3x3_n10 smooth_5x5_n10 smooth_7x7_n10
 
-parallel -j 8 --no-notice Rscript validate_map.R --in_dir     {1}/rep_prodes_{2}/results_{3}/smooth_{4}/prodes_reproduction --label_file {1}/rep_prodes_{2}/results_dl/int_labels.csv --out_dir    {1}/rep_prodes_{2}/results_{3}/smooth_{4}/prodes_reproduction/validation ::: /home/alber/Documents/data/experiments/prodes_reproduction/03_classify ::: 40 41 42 50 51 52 ::: dl rf svm vote ::: 3x3_n10 5x5_n10 7x7_n10 
+# Compute accuraries for smoothed classifications.
+parallel -j 8 --no-notice Rscript validate_map.R --in_dir {1}/rep_prodes_{2}/results_{3}/smooth_{4} --label_file {1}/rep_prodes_{2}/results_dl/int_labels.csv --out_dir {1}/rep_prodes_{2}/results_{3}/smooth_{4}/validation ::: /home/alber/Documents/data/experiments/prodes_reproduction/03_classify ::: 40 41 42 50 51 52 ::: dl rf svm vote ::: 3x3_n10 5x5_n10 7x7_n10 9x9_n10 11x11_n10
+
+# Compute accuraries for non-smoothed classifications.
+parallel -j 8 --no-notice Rscript validate_map.R --in_dir {1}/rep_prodes_{2}/results_{3} --label_file {1}/rep_prodes_{2}/results_dl/int_labels.csv --out_dir {1}/rep_prodes_{2}/results_{3}/validation ::: /home/alber/Documents/data/experiments/prodes_reproduction/03_classify ::: 40 41 42 50 51 52 ::: dl rf svm
 
 echo "Finished"
 
