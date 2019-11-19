428d8c369fab54f6aa9db6ae7e5823e628e29e60
albhasan <albhasan@gmail.com>
2019-11-19 16:10:39 -0200

    Added spatial and thematic filters to the sampling.

--- inst/examples/prepare_sen2agri_samples.R
+++ inst/examples/prepare_sen2agri_samples.R
@@ -3,13 +3,14 @@
 library(dplyr)
 
 #---- configuration ----
-
+set.seed(666)
 base_dir <- "/home/alber/Documents/data/experiments/prodes_reproduction"
 sentinel_shp <- base_dir %>%
     file.path() %>%
     ensurer::ensure_that(dir.exists(.))
 sentinel_tiles <- c("19LGK")
 out_dir <- "/home/alber/Documents/data/experiments/prodes_reproduction/data/vector/sen2agri/samples"
+prodes_years <- 2017:2019
 
 #----- utilitary functions ----
 
@@ -19,14 +20,18 @@ read_prodes <- function(x){
     # Sen2agri legend.
     legend_tb <- "/home/alber/Documents/ghProjects/sits.prodes/inst/extdata/esa_cci_lc_legend.csv" %>%
         readr::read_csv() %>%
-        dplyr::mutate(label = ifelse(is.na(Global_label), Regional_label, Global_label),
-                      value = ifelse(is.na(Global_value), Regional_value, Global_value)) %>%
+        dplyr::mutate(label = ifelse(is.na(Global_label), Regional_label, 
+                                     Global_label),
+                      value = ifelse(is.na(Global_value), Regional_value, 
+                                     Global_value)) %>%
         dplyr::select(sen2agri_label = label, sen2agri_id = value) %>%
-        dplyr::mutate(CROP = as.integer(stringr::str_detect(sen2agri_label, "Cropland")),
+        dplyr::mutate(CROP = as.integer(stringr::str_detect(sen2agri_label, 
+                                                            "Cropland")),
                       LC = stringr::str_sub(sen2agri_label, 1, 70),
                       CODE = as.integer(sen2agri_id),
                       IRRIGATION = 0L) %>%
-        ensurer::ensure_that(!any(sapply(., is.na)), err_desc = "Inconsistent legend.")
+        ensurer::ensure_that(!any(sapply(., is.na)), 
+                             err_desc = "Inconsistent legend.")
 
     # Map PRODES' classes to Sen2agri. 
     data(prodes_labels, package = "sits.prodes")
@@ -110,15 +115,33 @@ sentinel_tb <- sentinel_tiles %>%
     })) %>%
     dplyr::mutate(bounding_box = purrr::map(sf, get_bbox),
                   prodes = purrr::map(bounding_box, intersect_prodes, prodes_tb),
-                  out_shp = file.path(out_dir, stringr::str_c("BR_", tile , "_LC_II_201911.shp"))) %>%
+                  out_shp = file.path(out_dir, stringr::str_c("BR_", tile , 
+                                                              "_LC_II_YEARMONTH.shp"))) %>%
     # Format the PRODES data to fit the Sen2agri's sample schema.
-    dplyr::mutate(samples_sf = purrr::map2(.$prodes, .$out_shp, function(prodes, out_shp){
-        prodes %>%
-            dplyr::mutate(ID = dplyr::row_number()) %>%
-            dplyr::select(ID, CROP, LC, CODE, IRRIGATION) %>%
-            dplyr::group_by(CODE) %>%
-            dplyr::sample_n(10) %>%
-            dplyr::ungroup() %>%
-            sf::st_write(out_shp)
-    }))
+    dplyr::mutate(samples_sf = purrr::pmap(dplyr::select(., prodes, bounding_box, out_shp), 
+                                           function(prodes, bounding_box, out_shp, prodes_years){
+        #is_within_bb <- function(x, bounding_box){
+        #    sf::st_within(x, bounding_box, sparse = FALSE)[1,]
+        #}
+        bb_wgs84 <- bounding_box %>%
+            sf::st_transform(crs = 4326) %>%
+            sf::st_buffer(-0.1)
+        for(pyear in prodes_years){
+            s2a_shp <- stringr::str_replace(out_shp, pattern = "_YEARMONTH", 
+                                            paste0('_', pyear, "11"))
+            prodes %>%
+                dplyr::mutate(ID = dplyr::row_number()) %>%
+                dplyr::filter(class_name %in% c("FLORESTA", "NAO_FLORESTA", 
+                                                "NAO_FLORESTA2", 
+                                                paste0('d', pyear)),
+                              areameters > 100000) %>%
+                #dplyr::filter(is_within_bb(., bb_wgs84)) %>%
+                sf::st_intersection(y = bb_wgs84) %>%
+                dplyr::select(ID, CROP, LC, CODE, IRRIGATION) %>%
+                dplyr::group_by(CODE) %>%
+                dplyr::sample_n(20) %>%
+                dplyr::ungroup() %>%
+                sf::st_write(s2a_shp)
+        }
+    }, prodes_years))
 
