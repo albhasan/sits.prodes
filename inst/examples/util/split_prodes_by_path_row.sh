1b9046a2c39eed59abf4c37b1187fa7761499cb9
albhasan <albhasan@gmail.com>
2020-02-24 12:05:40 -0300

    Updated tile ids.

--- inst/examples/util/split_prodes_by_path_row.sh
+++ inst/examples/util/split_prodes_by_path_row.sh
@@ -14,35 +14,39 @@ mkdir "$WD"/tiled
 tar -xzf $PRODES_TAR --directory "$WD"
 
 # get the landsat scenes of AOI
-parallel -j6 ogr2ogr -skipfailures -where \"pathrow=\'{1}\'\" "$WD"/tiled/{2/.}_{1}.shp {2} ::: 001066 001067 002066 002067 225063 226062 226063 226064 226068 227067 227068 233067 ::: "$PRODES_SHP"
+parallel -j6 ogr2ogr -skipfailures -where \"pathrow=\'{1}\'\" "$WD"/tiled/{2/.}_{1}.shp {2} ::: 00166 00167 00266 00267 22563 22662 22663 22664 22668 22767 22768 23367 ::: "$PRODES_SHP"
 
 # fix topological issues
 parallel ogr2ogr -sql '"SELECT ST_Buffer(geometry, 0.0), linkcolumn, uf, pathrow, scene_id, mainclass, class_name, dsfnv, julday, view_date, ano, areameters FROM {1/.}"' -dialect SQLite "$WD"/buffer/{1/} {1} ::: $(find "$WD"/tiled -maxdepth 1 -type f -name "*.shp")
 
 # rename 
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_001_066.shp "$WD"/tiled/PDigital2017_AMZ_pol_001066.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_001_067.shp "$WD"/tiled/PDigital2017_AMZ_pol_001067.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_002_066.shp "$WD"/tiled/PDigital2017_AMZ_pol_002066.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_002_067.shp "$WD"/tiled/PDigital2017_AMZ_pol_002067.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_225_063.shp "$WD"/tiled/PDigital2017_AMZ_pol_225063.shp 
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_226_062.shp "$WD"/tiled/PDigital2017_AMZ_pol_226062.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_226_063.shp "$WD"/tiled/PDigital2017_AMZ_pol_226063.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_226_068.shp "$WD"/tiled/PDigital2017_AMZ_pol_226068.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_227_067.shp "$WD"/tiled/PDigital2017_AMZ_pol_227067.shp
-ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_227_068.shp "$WD"/tiled/PDigital2017_AMZ_pol_227068.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_001_066.shp "$WD"/tiled/PDigital2017_AMZ_pol_00166.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_001_067.shp "$WD"/tiled/PDigital2017_AMZ_pol_00167.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_002_066.shp "$WD"/tiled/PDigital2017_AMZ_pol_00266.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_002_067.shp "$WD"/tiled/PDigital2017_AMZ_pol_00267.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_225_063.shp "$WD"/tiled/PDigital2017_AMZ_pol_22563.shp 
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_226_062.shp "$WD"/tiled/PDigital2017_AMZ_pol_22662.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_226_063.shp "$WD"/tiled/PDigital2017_AMZ_pol_22663.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_226_064.shp "$WD"/tiled/PDigital2017_AMZ_pol_22664.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_226_068.shp "$WD"/tiled/PDigital2017_AMZ_pol_22668.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_227_067.shp "$WD"/tiled/PDigital2017_AMZ_pol_22767.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_227_068.shp "$WD"/tiled/PDigital2017_AMZ_pol_22768.shp
+ogr2ogr -f "ESRI Shapefile" "$WD"/tiled/PDigital2017_AMZ_pol_233_067.shp "$WD"/tiled/PDigital2017_AMZ_pol_23367.shp
 
 # cleaning
 rm -rf "$WD"/prodes_2017
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_001066.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_001067.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_002066.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_002067.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_225063.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_226062.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_226063.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_226068.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_227067.*
-rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_227068.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_00166.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_00167.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_00266.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_00267.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_22563.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_22662.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_22663.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_22664.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_22668.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_22767.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_22768.*
+rm -rf "$WD"/tiled/PDigital2017_AMZ_pol_23367.*
 
 exit 0
 
